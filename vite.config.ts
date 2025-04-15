7d2c1a229a5cb08ef6c289f8c40c9b86e384cd64
Dennis Ammen <dennis.ammen@outlook.com>
2025-04-15 16:39:11 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+import path from 'path';
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    define: {
+      __APP_TITLE__: JSON.stringify(env.VITE_APP_TITLE),
+    },
+  };
+});
