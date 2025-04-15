c62e69485299b7201a9d73f505cbe3fcb780ca16
Dennis Ammen <dennis.ammen@outlook.com>
2025-04-15 17:02:26 +0200

    Installed pdftron/webviewer and vite-plugin-static-copy

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 import path from 'path';
 
@@ -8,7 +9,17 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      viteStaticCopy({
+        targets: [
+          {
+            src: 'node_modules/@pdftron/webviewer/public/*',
+            dest: 'lib/webviewer',
+          },
+        ],
+      }),
+    ],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
